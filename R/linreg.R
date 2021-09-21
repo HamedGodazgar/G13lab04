ffa217aae852ece31b23d02329273e600d5d0439
HamedGodazgar <hamed.godazgar@gmail.com>
2021-09-21 23:04:26 +0330

    buf fixed

--- R/linreg.R
+++ R/linreg.R
@@ -7,14 +7,8 @@
 #' print(mod_object)
 #' resid(mod_object)
 #' @export linreg
-#' @exportS3Method 
-#' print()
-#' plot()
-#' resid()
-#' pred()
-#' coef()
-#' summary()
 #' @import ggplot2
+#' @import cowplot
 #' @name linreg
 #' @param formula
 #' @param data
@@ -57,20 +51,22 @@ function(formula , data) {
   ## t-value
   tval <- betahat / sqrt(diag(v))
   yy <- data[all.vars(formula)[1]]
-  
+  std <- sqrt(abs(resi))
   ## return
   return_obj <- list(Coefficients = betahat, fitted.values = yhat,
                      residuals = resi, degfreedom = dof,
-                     vcov = v, varr = sqrt(se2), t_value = tval,call=match.call(),y=yy)
+                     vcov = v, varr = sqrt(se2), t_value = tval,
+                     call=match.call(), y=yy, standardized= std)
   
   class(return_obj) <- 'linreg'
   return(return_obj)
   
 }
 
+mod_object <- linreg(Petal.Length~Species, data=iris)
+linreg_mod <- linreg(Petal.Length~Sepal.Width+Sepal.Length, data=iris)
 
-
-
+# print method
 print.linreg <- function(obj){
   cat("Call:\n")
   print(obj$call)
@@ -78,65 +74,89 @@ print.linreg <- function(obj){
   print(obj$Coefficients)
   
 }
-print(mod_object)
 
+# resid method
 resid.linreg <- function(obj){
   return(obj$residuals)
   
 }
-resid(mod_object)
+
+# pred method
 pred <- function(obj){
   return(obj$fitted.values)
   
 }
-pred(mod_object)
 
+# coef method
 coef.linreg <- function(obj){
-  names(obj$Coefficients)
-  
-  
+  #names(obj$Coefficients)
+  obj$Coefficients
 }
-coef(mod_object)
 
+# summary method
 
-
+summary.linreg <- function(obj){
+  
+  out <- cbind(1,obj$Coefficients)
+  return(out)
+}
+summary(linreg_mod)
 
 summary.linreg <- function(obj)
 {
   se <- sqrt(diag(obj$vcov))
   tval <- obj$t_value
-  TAB <- cbind(Estimate = obj$Coefficients,
+  estt <- obj$Coefficients
+  TAB <- cbind(Estimate = estt,
                StdErr = se,
                t.value = tval,
                p.value = 2*pt(-abs(tval), df=obj$degfreedom))
-  res <- list(call=obj$call,
-              coefficients=TAB)
-  class(res) <- "summary.linreg"
+  colnames(TAB) <- c('Estimate', 'Std. Error', 't value', 'Pr(>|t|)')
+  res <- TAB
+  #class(res) <- "summary.linreg"
   print(res)
   cat("\nResidual standard error:" , obj$varr ,"on", obj$degfreedom ,"degrees of freedom")
 }
+summary(linreg_mod)
 summary(mod_object)
 
 
-
-
-
-
-
-plot.linreg <- function(obj) {
+# plot method
+library(cowplot)
+plot.linreg <- function(object) {
   library(ggplot2)
-  par(mfrow=c(2,1))
-  ggplot(obj$Petal.Length,aes(x = obj$fitted.values, y = obj$residuals),
-         color="steelblue")+
+  bp <- ggplot(data=iris,aes(x = object$fitted.values, y = object$residuals,label=Species))+ 
     geom_point(shape=1,size=4) + geom_smooth(method="lm", colour="red",
-                                             se = FALSE)+
+                                             se = FALSE)+ 
     labs(title="Residuals vs Fitted",x="Fitted values
       linreg(Petal.Length~Species, data = iris)", y="Residuals")+
     theme(plot.title = element_text(hjust = 0.5),
-          panel.background = element_rect(fill = "white", colour = "grey50"))
+          panel.background = element_rect(fill = "white", colour = "grey50"))+
+    
+    geom_text(aes(label=ifelse(object$residuals>1&object$residuals<(-1),
+                               as.character(Species),'')),hjust=0,vjust=0)
   
+  sp <-   ggplot(data=iris,aes(x = object$fitted.values, y = object$standardized))+ 
+    geom_point(shape=1,size=4) + geom_smooth(method="lm", colour="red",
+                                             se = FALSE)+ 
+    labs(title="Scale−Location",x="Fitted values
+  linreg(Petal.Length~Species)", y=expression(sqrt(abs("Standard resduals"))))+
+    theme(plot.title = element_text(hjust = 0.5),panel.background = 
+            element_rect(fill = "white", colour = "grey50"))
   
-  
+  plot_grid(sp, bp, ncol = 1, nrow = 2)
   
 }
-plot(mod_object)
+
+
+
+
+
+
+
+
+
+
+
+
+
